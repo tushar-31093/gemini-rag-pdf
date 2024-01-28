ee7f4fa856dab194cf2aa926b6ee51d03e3145b6
Tushar Bhatnagar <40886260+tushar-31093@users.noreply.github.com>
2024-01-28 21:07:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def get_text_chunks(text):
     return chunks
 
 def get_vector_store(text_chunks):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key = gemini_key)
     vector_store = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     vector_store.save_local("faiss_index")
 
@@ -44,7 +44,7 @@ def get_conversation_chain():
         Question:\n{question}\n
         Answer:
     """
-    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
+    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3, google_api_key = gemini_key)
     prompt = PromptTemplate(template=prompt_template, input_variables=['context', 'question'])
     chain = load_qa_chain(model, chain_type='stuff', prompt=prompt)
     return chain
