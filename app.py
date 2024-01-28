3d5236efd527cf0496fa0667eb00a22884f0b2fe
Tushar Bhatnagar <40886260+tushar-31093@users.noreply.github.com>
2024-01-28 20:23:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,9 @@ def clear_chat_history():
 
 def user_input(user_question):
     embeddings = GoogleGenerativeAIEmbeddings(
-        model="models/embedding-001")  # type: ignore
+        model="models/embedding-001",
+    client=genai,
+    temperature=0.3,)  # type: ignore
 
     new_db = FAISS.load_local("faiss_index", embeddings)
     docs = new_db.similarity_search(user_question)
