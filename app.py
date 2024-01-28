1dbfdf8bd9a190a8c53043b3f19b19e7771904fd
Tushar Bhatnagar <40886260+tushar-31093@users.noreply.github.com>
2024-01-28 20:49:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,9 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from htmlTemplates import css, bot_template, user_template
 
-load_dotenv()
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+gemini_key = st.secrets["GOOGLE_API_KEY"]
+genai.configure(api_key=gemini_key)
 
 def get_pdf_text(pdf_docs):
     text = ""
