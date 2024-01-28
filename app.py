32816a7f77d5c350b404b92150731fd4842a853f
Tushar Bhatnagar <40886260+tushar-31093@users.noreply.github.com>
2024-01-28 22:04:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
-import streamlit as st
-from dotenv import load_dotenv
+import os
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import streamlit as st
 import google.generativeai as genai
 from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
-from htmlTemplates import css, bot_template, user_template
-
+from dotenv import load_dotenv
 
 gemini_key = st.secrets["geminiDetails"]["GOOGLE_API_KEY"]
 genai.configure(api_key=gemini_key)
+
+# read all pdf files and return text
+
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -23,77 +24,120 @@ def get_pdf_text(pdf_docs):
             text += page.extract_text()
     return text
 
-def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=10000,
-        chunk_overlap=1000
-    )
-    chunks = text_splitter.split_text(text)
-    return chunks
+# split text into chunks
 
-def get_vector_store(text_chunks):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key = gemini_key)
-    vector_store = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+
+def get_text_chunks(text):
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=10000, chunk_overlap=1000)
+    chunks = splitter.split_text(text)
+    return chunks  # list of strings
+
+# get embeddings for each chunk
+
+
+def get_vector_store(chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model="models/embedding-001", google_api_key = gemini_key)  # type: ignore
+    vector_store = FAISS.from_texts(chunks, embedding=embeddings)
     vector_store.save_local("faiss_index")
 
-def get_conversation_chain():
+
+def get_conversational_chain():
     prompt_template = """
-        Answer the question clear and precise. If not provided the context return the result as
-        "Sorry I dont know the answer", don't provide the wrong answer.
-        Context:\n {context}?\n
-        Question:\n{question}\n
-        Answer:
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
     """
-    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3, google_api_key = gemini_key)
-    prompt = PromptTemplate(template=prompt_template, input_variables=['context', 'question'])
-    chain = load_qa_chain(model, chain_type='stuff', prompt=prompt)
+
+    model = ChatGoogleGenerativeAI(model="gemini-pro",
+                                   google_api_key = gemini_key,
+                                   temperature=0.3,
+                                   )
+    prompt = PromptTemplate(template=prompt_template,
+                            input_variables=["context", "question"])
+    chain = load_qa_chain(llm=model, chain_type="stuff", prompt=prompt)
     return chain
 
+
+def clear_chat_history():
+    st.session_state.messages = [
+        {"role": "assistant", "content": "upload some pdfs and ask me a question"}]
+
+
 def user_input(user_question):
-    embeddings = GoogleGenerativeAIEmbeddings(model='models/embedding-001', google_api_key = gemini_key)
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model="models/embedding-001", google_api_key = gemini_key)  # type: ignore
+
     new_db = FAISS.load_local("faiss_index", embeddings)
     docs = new_db.similarity_search(user_question)
 
-    chain = get_conversation_chain()
+    chain = get_conversational_chain()
+
     response = chain(
-        {"input_documents": docs, "question": user_question},
-        return_only_outputs=True
+        {"input_documents": docs, "question": user_question}, return_only_outputs=True, )
+
+    print(response)
+    return response
+
+
+def main():
+    st.set_page_config(
+        page_title="Gemini PDF Chatbot",
+        page_icon="🤖"
     )
-    st.write(user_template.replace("{{MSG}}", response["output_text"]), unsafe_allow_html=True)
 
-def main():
-    st.set_page_config(page_title="Chat with multiple PDFs",
-                       page_icon=":books:")
-    st.write(css, unsafe_allow_html=True)
-    
-def main():
-    st.set_page_config(page_title="Chat with multiple PDFs",
-                       page_icon=":books:")
-    st.write(css, unsafe_allow_html=True)
-
-    if "conversation" not in st.session_state:
-        st.session_state.conversation = None
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = None
-
-    st.header("Chat with multiple PDFs with Gemini Pro :books:")
-    user_question = st.text_input("Ask a Question from the PDF Files")
-
-    if user_question:
-        user_input(user_question)
-
+    # Sidebar for uploading PDF files
     with st.sidebar:
+        st.title("Menu:")
         pdf_docs = st.file_uploader(
-            "Upload your PDF Files and Click on Process", 
-            accept_multiple_files=True
-        )
-        if st.button("Process"):
+            "Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
             with st.spinner("Processing..."):
                 raw_text = get_pdf_text(pdf_docs)
                 text_chunks = get_text_chunks(raw_text)
                 get_vector_store(text_chunks)
-                st.session_state.conversation = get_conversation_chain()
                 st.success("Done")
+
+    # Main content area for displaying chat messages
+    st.title("Chat with PDF files using Gemini🤖")
+    st.write("Welcome to the chat!")
+    st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+
+    # Chat input
+    # Placeholder for chat messages
+
+    if "messages" not in st.session_state.keys():
+        st.session_state.messages = [
+            {"role": "assistant", "content": "upload some pdfs and ask me a question"}]
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+    if prompt := st.chat_input():
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.write(prompt)
+
+    # Display chat messages and bot response
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking..."):
+                response = user_input(prompt)
+                placeholder = st.empty()
+                full_response = ''
+                for item in response['output_text']:
+                    full_response += item
+                    placeholder.markdown(full_response)
+                placeholder.markdown(full_response)
+        if response is not None:
+            message = {"role": "assistant", "content": full_response}
+            st.session_state.messages.append(message)
+
 
 if __name__ == "__main__":
     main()
